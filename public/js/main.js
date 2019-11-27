718cc154b5d392b791c421f8bdb9efb1a1cd2fc8
DMC09 <Dharminlive12@live.com>
2019-11-26 21:58:49 -0600

    Added nav menu animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,14 @@
 burger = document.querySelector('.hamburger')
 menu = document.querySelector('.nav-menu')
+layout = document.querySelectorAll('footer , #main-content')
+console.log(layout);
 
 
 const navAni = () => {
   console.log('function is starting');
   menu.classList.toggle('navSlide')
+  layout.forEach((obj) => {
+    obj.classList.toggle('darken')
+  })
+
 }
