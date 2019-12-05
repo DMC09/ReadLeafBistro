01d9a73716418824a0def2a34c2a6d8c88c2f265
DMC09 <Dharminlive12@live.com>
2019-12-04 21:50:13 -0600

    Javascript tab switching outline complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,13 @@
-burger = document.querySelector('.hamburger')
-menu = document.querySelector('.nav-menu')
-layout = document.querySelectorAll('footer , #main-content')
-console.log(layout);
+const burger = document.querySelector('.hamburger')
+const menu = document.querySelector('.nav-menu')
+const layout = document.querySelectorAll('footer , #main-content')
+const appContent = document.querySelector('.apps')
+const entContent = document.querySelector('.entrees')
+const desContent = document.querySelector('.desserts')
+const driContent = document.querySelector('.drinks')
+const tabContent = [appContent, entContent, desContent, driContent]
 
+// console.log(tabContent);
 
 const navAni = () => {
   console.log('function is starting');
@@ -10,5 +15,31 @@ const navAni = () => {
   layout.forEach((obj) => {
     obj.classList.toggle('darken')
   })
-
 }
+
+const reset = () => {
+  tabContent.forEach(element => {
+    element.classList.remove("tab-Content-active")
+  })
+}
+
+
+
+
+const appDrawer = () => {
+  reset()
+  appContent.classList.toggle("tab-Content-active")
+};
+
+const entreeDrawer = () => {
+  reset()
+  entContent.classList.toggle("tab-Content-active")
+};
+const dessertsDrawer = () => {
+  reset()
+  desContent.classList.toggle("tab-Content-active")
+};
+const drinksDrawer = () => {
+  reset()
+  driContent.classList.toggle("tab-Content-active")
+};
