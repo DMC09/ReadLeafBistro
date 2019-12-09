008538ab8290010fe008555b8463120ac662ab9a
DMC09 <Dharminlive12@live.com>
2019-12-08 18:22:41 -0600

    changed port

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const port = 8080
 const routes = require('./routes.js');
 
 
@@ -12,5 +12,5 @@ app.use("/", routes);
 app.use(express.static('public'));
 // template engine
 app.set('view engine', 'pug')
-// listening on port 3000
+// listening on port 8080
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
