93fb213b5f449b3948d6fdd8c218d61af77a3467
DMC09 <Dharminlive12@live.com>
2020-10-07 21:30:38 -0500

    rought footer section complete.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const path = require('path');
 
 // Routes
 app.use(express.static('/public'));
 app.use('/images', express.static('public/images'))
+app.use('/css', express.static('public/css'))
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/html/index.html'));
 });
