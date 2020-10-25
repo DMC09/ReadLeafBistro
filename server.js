b5a83ba790712f4fe61e6d0700866d764eabd033
DMC09 <Dharminlive12@live.com>
2020-10-25 15:33:45 -0500

    replaced favicons

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('/public'));
 app.use('/images', express.static('public/images'))
 app.use('/css', express.static('public/css'))
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/html/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
